20fffb9ad464b387ffcfd848922e837feb5dc76e
Tudor Roman <tudurom@gmail.com>
2018-03-09 20:45:06 +0200

    refactored nav component

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -9,28 +9,33 @@ import { basic_home } from 'react-icons-kit/linea/basic_home';
 import { basic_archive_full } from 'react-icons-kit/linea/basic_archive_full';
 import { arrows_hamburger1 } from 'react-icons-kit/linea/arrows_hamburger1';
 import { basic_gear } from 'react-icons-kit/linea/basic_gear';
+import { userOutline as user_outline } from 'react-icons-kit/typicons/userOutline';
+
+function NavLink({ href, icon, text, bottom }) {
+	let id = '';
+	if (bottom) {
+		id = style.bottom;
+	}
+
+	return (
+		<Link activeClassName={style.active} href={href} id={id}>
+			<Icon icon={icon} size={32} />
+			<span>{text}</span>
+		</Link>
+	);
+}
 
 export default class Navigation extends Component {
 	render() {
 		return (
 			<nav class={style.navigation}>
 				<img src={logo} class={style.logo} />
-				<Link activeClassName={style.active} href="/">
-					<Icon icon={basic_home} size={32} />
-					<span>HOME</span>
-				</Link>
-				<Link activeClassName={style.active} href="/archive">
-					<Icon icon={basic_archive_full} size={32} />
-					<span>ARCHIVE</span>
-				</Link>
-				<Link activeClassName={style.active} href="/submissions">
-					<Icon icon={arrows_hamburger1} size={32} />
-					<span>MONITOR</span>
-				</Link>
-				<Link activeClassName={style.active} href="/settings" id={style.bottom}>
-					<Icon icon={basic_gear} size={32} />
-					<span>SETTINGS</span>
-				</Link>
+				<NavLink href="/" icon={basic_home} text="HOME" />
+				<NavLink href="/archive" icon={basic_archive_full} text="ARCHIVE" />
+				<NavLink href="/submissions" icon={arrows_hamburger1} text="MONITOR" />
+
+				<NavLink href="/login" icon={user_outline} text="LOGIN" />
+				<NavLink href="/settings" icon={basic_gear} bottom text="SETTINGS" />
 			</nav>
 		);
 	}
