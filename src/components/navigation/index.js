25bef86e7211340e85271daa4e7774e7c54786a4
Tudor Roman <tudurom@gmail.com>
2018-03-10 13:29:51 +0200

    refactor navigation

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -4,7 +4,7 @@ import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
 import logo from './logo.svg';
-import Ionicon from 'react-ionicons'
+import Ionicon from 'react-ionicons';
 
 function NavLink({ href, icon, text, bottom }) {
 	let id = '';
@@ -14,28 +14,48 @@ function NavLink({ href, icon, text, bottom }) {
 
 	return (
 		<Link activeClassName={style.active} href={href} id={id}>
-			<Ionicon icon={icon} fontSize={32} color="white"/>
+			<Ionicon icon={icon} fontSize="32px" color="white" />
 			<span>{text}</span>
 		</Link>
 	);
 }
 
-export default class Navigation extends Component {
-	render() {
-		return (
-			<nav class={style.navigation}>
-				<img src={logo} class={style.logo} />
-				<div class={style.section} id={style.middle}>
-					<NavLink href="/" icon="ios-home-outline" text="HOME" />
-					<NavLink href="/archive" icon="ios-filing-outline" text="ARCHIVE" />
-					<NavLink href="/submissions" icon="ios-list-box-outline" text="MONITOR" />
-				</div>
-
-				<div class={style.section} id={style.bottom}>
-					<NavLink href="/login" icon="ios-person-outline" text="LOGIN" />
-					<NavLink href="/settings" icon="ios-settings-outline" bottom text="SETTINGS" />
-				</div>
-			</nav>
-		);
+function NavGroup({ children, bottom }) {
+	let id = style.middle;
+	if (bottom) {
+		id = style.bottom;
 	}
+
+	return (
+		<div class={style.section} id={id}>
+			{children}
+		</div>
+	);
+}
+
+export default function Navigation() {
+	return (
+		<nav class={style.navigation}>
+			<img src={logo} class={style.logo} />
+			<NavGroup>
+				<NavLink href="/" icon="ios-home-outline" text="HOME" />
+				<NavLink href="/archive" icon="ios-filing-outline" text="ARCHIVE" />
+				<NavLink
+					href="/submissions"
+					icon="ios-list-box-outline"
+					text="MONITOR"
+				/>
+			</NavGroup>
+
+			<NavGroup bottom>
+				<NavLink href="/login" icon="ios-person-outline" text="LOGIN" />
+				<NavLink
+					href="/settings"
+					icon="ios-settings-outline"
+					bottom
+					text="SETTINGS"
+				/>
+			</NavGroup>
+		</nav>
+	);
 }
