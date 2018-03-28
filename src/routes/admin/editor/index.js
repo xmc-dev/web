62e4ccb8ddfbf72cb7cff204e7c00808b4ccec76
Ciprian Ionescu <me@ciprian-ionescu.com>
2018-03-29 02:12:49 +0300

    Admin homepage prototype.

--- src/routes/admin/editor/index.js
+++ src/routes/admin/editor/index.js
@@ -1,3 +1,11 @@
 import { Component } from 'preact';
 
-export default class Editor extends Component {}
+export default class Editor extends Component {
+	render() {
+		return (
+			<Container>
+				<Header>Page editor</Header>
+			</Container>
+		);
+	}
+}
